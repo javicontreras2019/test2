e4d212ce9cb67acd2a9f880a549548da01e13d71
Javi Contreras <javiecontreras@alumnos.uai.cl>
2019-07-07 20:36:44 -0400

    Tarea 1 final

--- listPid.c
+++ listPid.c
@@ -4,14 +4,7 @@
 #include "pstat.h"
 
 int main(){
-          printf(1, "Process scheduling statistics:\n");
-    printf(1, "Slot\tPID\tHigh\tLow\n");
-    struct pstat st;
-    getAllPids(&st);
-    int i;
-    for(i = 0; i < NPROC; i++)
-                  if(st.inuse[i])
-                           printf(1, "%d\t%d\t%d\t%d\t%s\n", i, st.pid[i], st.hticks[i], st.lticks[i],st.name[i]);
-        
+         
+        printf(1,"Procesos: %d\n", getprocs());
             exit();
 }
